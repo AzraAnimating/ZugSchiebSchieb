d68560cb34b677f4e0a5a68a2a16463d7fbb046e
Azra Animating <42267730+AzraAnimating@users.noreply.github.com>
2019-12-06 14:18:52 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,50 +1,59 @@
-public class nnController {
-    Stack<Waggon> gleis1 = new Stack<>();
-    Stack<Waggon> gleis2 = new Stack<>();
-    Stack<Waggon> gleis3 = new Stack<>();
-
-    public nnController(int pAnzahlWaggons){
-        for(int i = 0 ; i < pAnzahlWaggons ; i++)
-        {
-            int randomNum = new java.util.Random().nextInt(pAnzahlWaggons);
-            gleis1.push(new Waggon(randomNum));
+import java.util.*;
+//https://github.com/azraanimating/zugschiebschieb/
+public class Controller{
+        Stack<Waggon> gleis1 = new Stack<>();
+        Stack<Waggon> gleis2 = new Stack<>();
+        Stack<Waggon> gleis3 = new Stack<>();
+    
+        public Controller(int pAnzahlWaggons){
+            for(int i = 0 ; i < pAnzahlWaggons ; i++)
+            {
+                int randomNum = new java.util.Random().nextInt(pAnzahlWaggons);
+                gleis1.push(new Waggon(randomNum));
+            }
         }
-    }
-
-    public void sort(){
-        gleis2.push(gleis1.top());
-        gleis1.pop();
-        while (!gleis1.isEmpty()){
-            if(gleis1.top().getNumber() < gleis2.top().getNumber()){
-                gleis2.push(gleis1.top());
-                gleis1.pop();
-            } else {
-                while (!gleis2.isEmpty() && gleis1.top().getNumber() > gleis2.top().getNumber()){
-                    gleis3.push(gleis2.top());
-                    gleis2.pop();
-                }
-                gleis2.push(gleis1.top());
-                gleis1.pop();
-                while (!gleis3.isEmpty()){
-                    gleis2.push(gleis3.top());
-                    gleis3.pop();
+    
+        public void sort()
+        {
+            gleis2.push(gleis1.top());
+            gleis1.pop();
+            while (!gleis1.isEmpty())
+            {
+                if(gleis1.top().getNumber() < gleis2.top().getNumber())
+                {
+                    gleis2.push(gleis1.top());
+                    gleis1.pop();
+                } 
+                else 
+                {
+                    while (!gleis2.isEmpty() && gleis1.top().getNumber() > gleis2.top().getNumber())
+                    {
+                        gleis3.push(gleis2.top());
+                        gleis2.pop();
+                    }
+                    gleis2.push(gleis1.top());
+                    gleis1.pop();
+                    while (!gleis3.isEmpty())
+                    {
+                        gleis2.push(gleis3.top());
+                        gleis3.pop();
+                    }
                 }
             }
         }
-    }
-
-    public void output()
-    {
-        while(!gleis1.isEmpty())
+    
+        public void output()
         {
-            System.out.println(gleis1.top().getNumber());
-            gleis2.push(gleis1.top());
-            gleis1.pop();
+            while(!gleis1.isEmpty())
+            {
+                System.out.println(gleis1.top().getNumber());
+                gleis2.push(gleis1.top());
+                gleis1.pop();
+            }
+            while(!gleis2.isEmpty())
+            {
+                gleis1.push(gleis2.top());
+                gleis2.pop();
+            }
         }
-        while(!gleis2.isEmpty())
-        {
-            gleis1.push(gleis2.top());
-            gleis2.pop();
-        }
-    }
 }
