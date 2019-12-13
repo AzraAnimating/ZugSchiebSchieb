7714d7639a5fd12608a7fc9068cdf5cb2c13cde8
Azra Animating <42267730+AzraAnimating@users.noreply.github.com>
2019-12-13 15:37:54 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -4,40 +4,92 @@ public class Controller{
         Stack<Waggon> gleis1 = new Stack<>();
         Stack<Waggon> gleis2 = new Stack<>();
         Stack<Waggon> gleis3 = new Stack<>();
+        GUI_project gui = new GUI_project(this);
     
         public Controller(int pAnzahlWaggons){
             for(int i = 0 ; i < pAnzahlWaggons ; i++)
             {
-                int randomNum = new java.util.Random().nextInt(pAnzahlWaggons);
-                gleis1.push(new Waggon(randomNum));
+                int lrandomNum = new java.util.Random().nextInt(pAnzahlWaggons);
+                gleis1.push(new Waggon(lrandomNum));
+                gui.addToGleis(1, lrandomNum);
             }
+        }
+        
+        public void addWaggon(int pNummer)
+        {
+            gleis1.push(new Waggon(pNummer));
         }
     
         public void sort()
         {
             gleis2.push(gleis1.top());
+            gui.addToGleis(2, gleis1.top().getNumber());
             gleis1.pop();
+            gui.removeFromGleis(1);
             while (!gleis1.isEmpty())
             {
                 if(gleis1.top().getNumber() < gleis2.top().getNumber())
                 {
                     gleis2.push(gleis1.top());
+                    gui.addToGleis(2, gleis1.top().getNumber());
                     gleis1.pop();
+                    gui.removeFromGleis(1);
                 } 
                 else 
                 {
                     while (!gleis2.isEmpty() && gleis1.top().getNumber() > gleis2.top().getNumber())
                     {
                         gleis3.push(gleis2.top());
+                        gui.addToGleis(3, gleis2.top().getNumber());
                         gleis2.pop();
+                        gui.removeFromGleis(2);
                     }
                     gleis2.push(gleis1.top());
+                    gui.addToGleis(2, gleis1.top().getNumber());
                     gleis1.pop();
+                    gui.removeFromGleis(1);
                     while (!gleis3.isEmpty())
                     {
                         gleis2.push(gleis3.top());
+                        gui.addToGleis(2, gleis3.top().getNumber());
                         gleis3.pop();
+                        gui.removeFromGleis(3);
                     }
+                }
+            }
+        }
+        
+        public void singleStep()
+        {
+            if(!gleis1.isEmpty())
+            {
+                if(!gleis2.isEmpty())
+                {
+                    if(gleis1.top().getNumber() < gleis2.top().getNumber())
+                        {
+                            gleis2.push(gleis1.top());
+                            gleis1.pop();
+                        } 
+                        else 
+                        {
+                            while (!gleis2.isEmpty() && gleis1.top().getNumber() > gleis2.top().getNumber())
+                            {
+                                gleis3.push(gleis2.top());
+                                gleis2.pop();
+                            }
+                            gleis2.push(gleis1.top());
+                            gleis1.pop();
+                            while (!gleis3.isEmpty())
+                            {
+                                gleis2.push(gleis3.top());
+                                gleis3.pop();
+                            }
+                        }
+                } 
+                else
+                {
+                    gleis2.push(gleis1.top());
+                    gleis1.pop();
                 }
             }
         }
@@ -46,7 +98,7 @@ public class Controller{
         {
             while(!gleis1.isEmpty())
             {
-                System.out.println(gleis1.top().getNumber());
+                gui.addToGleis(1, gleis1.top().getNumber());
                 gleis2.push(gleis1.top());
                 gleis1.pop();
             }
@@ -55,5 +107,9 @@ public class Controller{
                 gleis1.push(gleis2.top());
                 gleis2.pop();
             }
+        }
+        
+        public void testClear(){
+            gui.removeFromGleis(1);
         }
 }
